c728231fcba142dea2236f6e71582c8c6b6249a5
Keerikadan03 <fenseraju@gmail.com>
2023-04-11 16:17:14 +0530

    completed buttons in slider

--- src/components/Residencies.jsx
+++ src/components/Residencies.jsx
@@ -15,20 +15,20 @@ const Residencies = () => {
           <span className="primaryText">Popular Residencies</span>
         </div>
         <Swiper {...sliderSettings}>
-            {
-                data.map((card,i) => (
-                    <SwiperSlide key = {i}>
-                        <div className="flexColStart r-card">
-                            <img src={card.image} alt="home" />
-                            <span className="secondaryText r-price">
-                                <span style={{color: "orange"}}>$</span><span>{card.price}</span>
-                            </span>
-                            <span className="primaryText">{card.name}</span>
-                            <span className="secondaryText">{card.detail}</span>
-                        </div>
-                    </SwiperSlide>
-                ))
-            }
+        <SliderButtons />
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>$</span>
+                  <span>{card.price}</span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
@@ -36,3 +36,13 @@ const Residencies = () => {
 };
 
 export default Residencies;
+
+const SliderButtons = () => {
+    const swiper = useSwiper();
+    return (
+      <div className="flexCenter r-buttons">
+        <button onClick={() => swiper.slidePrev()}>&lt;</button>
+        <button onClick={() => swiper.slideNext()}>&gt;</button>
+      </div>
+    )
+};
--- src/utils/common.js
+++ src/utils/common.js
@@ -3,19 +3,15 @@ export const sliderSettings = {
     spaceBetween : 50,
     breakpoints:{
         480:{
-            width: 480,
             slidesPerView : 1
         },
         600:{
-            width: 600,
             slidesPerView : 2
         },
         750:{
-            width: 700,
             slidesPerView : 3
         },
         1100:{
-            width: 1100,
             slidesPerView : 4
         }
     }
