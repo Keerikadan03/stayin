9e0b6b7324b1d0d4ffe031dda308432ac56d99a3
hrishikeshr1 <hrishikeshrofficial@gmail.com>
2023-04-11 13:23:31 +0530

    created Hero  component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/Header";
 import Companies from "./components/Companies";
 import Residencies from "./components/Residencies";
 import Hero from "./components/Hero";
+import './styles/App.css'
 
 function App() {
   return (
-    <div>
-      <div className="App">
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
         <Header />
         <Hero />
       </div>
